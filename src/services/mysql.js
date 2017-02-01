74324d41c7e72ae021868e1a39cc17f8701d24d6
Aaron Magid <ahm64@case.edu>
2017-01-31 19:14:07 -0500

    feat(db): Added mysql service for db connections.

---
+++ src/services/mysql.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize');
+const config = require('../../config');
+const logger = require('./logger');
+
+module.exports = {
+    connect,
+    connection
+};
+
+let _connection;
+
+function connect(force = false) {
+    if (!_connection || force) {
+        const db = config.get().db;
+        _connection = new Sequelize(db.name, db.username, db.password, {
+                host: 'localhost',
+                dialect: 'mysql',
+                pool: {
+                    max: 1,
+                    min: 0,
+                    idle: 10000
+                }
+            });
+        return _connection.authenticate()
+            .then(function (err) {
+                logger.info('Connection has been established successfully.');
+                return _connection;
+            })
+            .catch(function (err) {
+                logger.error('Unable to connect to the database:', err);
+                throw err;
+            });
+    } else {
+        return _connection;
+    }
+}
+
+function connection() {
+    return _connection;
+}
