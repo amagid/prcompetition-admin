2e916372bd77577b75ee3c6aa8045fd4983ae246
Aaron Magid <ahm64@case.edu>
2017-02-01 15:15:54 -0500

    feat(validation): Added route to participants for testing.

--- src/routes/api/participants/index.js
+++ src/routes/api/participants/index.js
@@ -1,3 +1,5 @@
+const validation = require('../../../middlewares/validation');
+const validators = require('./validators');
 const Participants = require('../../../models/Participants');
 
 module.exports = routes;
@@ -7,4 +9,8 @@ function routes(router) {
     router.get('/', (req, res) => res.promise(Participants.findAll()));
 
     router.get('/summary', (req, res) => res.promise(Participants.getSummary()));
+
+    router.get('/:id',
+        validation(validators.validateGetOne),
+        (req, res) => res.promise(Participants.findById(req.params.id)));
 }
