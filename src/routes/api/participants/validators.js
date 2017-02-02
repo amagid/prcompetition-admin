cc5ae6ebae9ffb7aea7bf7ce2d13e470ca03862e
Aaron Magid <ahm64@case.edu>
2017-02-02 14:14:34 -0500

    feat(CRUD): Added validation to basic CRUD routes.

--- src/routes/api/participants/validators.js
+++ src/routes/api/participants/validators.js
@@ -1,9 +1,32 @@
 module.exports = {
-    validateGetOne
+    validateGetOne,
+    validatePost,
+    validateDelete,
+    validatePatch
 };
 
 function validateGetOne(input) {
     input.check('id').notEmpty().isInt();
 
     input.sanitize('id').toInt();
+}
+
+function validatePost(input) {
+    input.check('name').notEmpty();
+    input.check('caseid').notEmpty().matches(/[a-z]{1,3}[0-9]*/);
+}
+
+function validateDelete(input) {
+    input.check('id').notEmpty().isInt();
+    
+    input.sanitize('id').toInt();
+}
+
+function validatePatch(input) {
+    input.check('id').notEmpty().isInt();
+    input.check('updates.name').optional();
+    input.check('updates.caseid').optional().matches(/[a-z]{1,3}[0-9]*/);
+
+    input.sanitize('id').toInt();
+    input.sanitize('updates.caseid').whitelist('[a-z0-9]');
 }
--- src/routes/api/uploadTypes/validators.js
+++ src/routes/api/uploadTypes/validators.js
@@ -1,9 +1,34 @@
 module.exports = {
-    validateGetOne
+    validateGetOne,
+    validatePost,
+    validateDelete,
+    validatePatch
 };
 
 function validateGetOne(input) {
     input.check('id').notEmpty().isInt();
 
     input.sanitize('id').toInt();
+}
+
+function validatePost(input) {
+    input.check('name').notEmpty();
+    input.check('value').notEmpty().isInt();
+    
+    input.sanitize('value').toInt();
+}
+
+function validateDelete(input) {
+    input.check('id').notEmpty().isInt();
+    
+    input.sanitize('id').toInt();
+}
+
+function validatePatch(input) {
+    input.check('id').notEmpty().isInt();
+    input.check('updates.name').optional();
+    input.check('updates.value').optional().isInt();
+
+    input.sanitize('id').toInt();
+    input.sanitize('updates.value').toInt();
 }
