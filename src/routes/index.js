250d65b24e56901c45a86a06cd2692ef8c3b4428
Aaron Magid <ahm64@case.edu>
2017-04-22 21:09:31 -0400

    deleted unfinished auth code since we don't have time to implement it.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const mountAPI = require('./api');
-const mountAuth = require('./auth');
 
 module.exports = addRoutes;
 
 function addRoutes(router) {
-    const api = express.Router(),
-        auth = express.Router();
+    const api = express.Router();
     mountAPI(api);
-    mountAuth(auth);
     
     router.use('/api', api);
-    router.use('/auth', auth);
 };
