b80f36c26dbb1985954ce3d31b8ad2415cfd0736
Aaron Magid <ahm64@case.edu>
2017-04-22 20:52:17 -0400

    Updated Sync file.

--- src/models/sync.js
+++ src/models/sync.js
@@ -1,18 +1,28 @@
-const Participants = require('./Participants');
-const Groups = require('./Groups');
-const Opportunities = require('./Opportunities');
+const AdditionalPoints = require('./AdditionalPoints');
+const AdTypes = require('./AdTypes');
+const Advertisements = require('./Advertisements');
 const Attendance = require('./Attendance');
-const UploadTypes = require('./UploadTypes');
+const Content = require('./Content');
+const Events = require('./Events');
+const FileTypes = require('./FileTypes');
+const Opportunities = require('./Opportunities');
+const Participants = require('./Participants');
 const Uploads = require('./Uploads');
+
+
 const Promise = require('bluebird');
 
 module.exports = sync;
 
 function sync() {
     return Participants.sync()
-        .then(() => Groups.sync)
+        .then(() => Events.sync)
+        .then(() => FileTypes.sync)
+        .then(() => Content.sync)
+        .then(() => AdTypes.sync)
+        .then(() => Advertisements.sync)
         .then(() => Opportunities.sync)
         .then(() => Attendance.sync)
-        .then(() => UploadTypes.sync)
-        .then(() => Uploads.sync);
+        .then(() => Uploads.sync)
+        .then(() => AdditionalPoints.sync);
 }
