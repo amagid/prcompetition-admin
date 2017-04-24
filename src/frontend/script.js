b06cec4d87f5230779b0852806cc169c7184a9b7
Aaron Magid <ahm64@case.edu>
2017-04-23 23:48:33 -0400

    Added Radio Buttons and fixed Checkbox structure.

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
-    $(".checkbox").click(function() {
-        if ($(this).hasClass("checked")) {
-            $(this).removeClass("checked");
+    $(".checkbox_container").click(function() {
+        var checkbox = $(this).find(".checkbox");
+        if (checkbox.hasClass("checked")) {
+            checkbox.removeClass("checked");
         } else {
-            $(this).addClass("checked");
+            checkbox.addClass("checked");
+        }
+    });
+
+    $(".radiobutton_container").click(function() {
+        if (!$(this).find(".radiobutton").hasClass("checked")) {
+            $(this).parent().find(".radiobutton").removeClass("checked");
+            $(this).find(".radiobutton").addClass("checked");
         }
     })
 });
